8db0c64f3b07f139c18cb910f03d248b46bf2a91
Mike Will <myQwil@gmail.com>
2023-09-14 04:59:58 -0400

    replace utils.subprocess with mp.command_native

--- trueautosub.lua
+++ trueautosub.lua
@@ -1,18 +1,15 @@
 -- requires subliminal, version 1.0 or newer
 -- default keybinding: b
-local utils = require 'mp.utils'
 
 -- download the subtitle
 local function load_sub_fn(force)
     subl = "/usr/local/bin/subliminal" -- use 'which subliminal' to find the path
     mp.msg.info("Searching subtitle")
     mp.osd_message("Searching subtitle")
-    t = {}
 
     force = force and "-f" or ""
-    t.args = {subl, "download", "-s", force, "-l", "en", mp.get_property("path")}
-    res = utils.subprocess(t)
-    if res.status == 0 then
+    args = {subl, "download", "-s", force, "-l", "en", mp.get_property("path")}
+    if mp.command_native({ name = "subprocess", args = args }).status == 0 then
         mp.commandv("rescan_external_files", "reselect")
         mp.msg.info("Subtitle download succeeded")
         mp.osd_message("Subtitle download succeeded")
