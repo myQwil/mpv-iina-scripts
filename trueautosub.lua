96f8ac0fd821a5b516e7ce1426b4bd88ede8a0ce
Mike Will <myQwil@gmail.com>
2023-09-14 04:05:47 -0400

    replace sleep function with mp.add_timeout

--- trueautosub.lua
+++ trueautosub.lua
@@ -2,21 +2,15 @@
 -- default keybinding: b
 local utils = require 'mp.utils'
 
---sleep function, so that subs aren't downloaded the instant a file is loaded
---taken from:http://lua-users.org/wiki/SleepFunction
-function sleep(s)
-    local ntime = os.time() + s
-    repeat until os.time() > ntime
-end
-
---original function, pressing b will trigger this and download the subtitle manually
-function load_sub_fn()
+-- download the subtitle
+local function load_sub_fn(force)
     subl = "/usr/local/bin/subliminal" -- use 'which subliminal' to find the path
     mp.msg.info("Searching subtitle")
     mp.osd_message("Searching subtitle")
     t = {}
 
-    t.args = {subl, "download", "-s", "-f", "-l", "en", mp.get_property("path")}
+    force = force and "-f" or ""
+    t.args = {subl, "download", "-s", force, "-l", "en", mp.get_property("path")}
     res = utils.subprocess(t)
     if res.status == 0 then
         mp.commandv("rescan_external_files", "reselect")
@@ -28,24 +22,7 @@ function load_sub_fn()
     end
 end
 
---added function, auto search for subs and download if not present, the way god intended :P
-function autosubs()
-    sleep(10)
-    subl = "/usr/local/bin/subliminal" -- use 'which subliminal' to find the path
-    mp.msg.info("Searching subtitle")
-    mp.osd_message("Searching subtitle")
-    t = {}
-    t.args = {subl, "download", "-s", "-l", "en", mp.get_property("path")}
-    res = utils.subprocess(t)
-    if res.status == 0 then
-        mp.commandv("rescan_external_files", "reselect")
-        mp.msg.info("Subtitle download succeeded")
-        mp.osd_message("Subtitle download succeeded")
-    else
-        mp.msg.warn("Subtitle download failed")
-        mp.osd_message("Subtitle download failed")
-    end
-end
+mp.add_key_binding("b", "auto_load_subs", function() load_sub_fn(true) end)
 
-mp.add_key_binding("b", "auto_load_subs", load_sub_fn)
-mp.register_event("file-loaded", autosubs)
+-- auto search for subs and download if not present, the way god intended :P
+mp.register_event("file-loaded", function() mp.add_timeout(10, load_sub_fn) end)
